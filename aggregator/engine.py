901299c5d6cb5eb1e3659162617fd5ba543a5019
raelix <raelix@hotmail.it>
2018-01-31 19:05:46 +0100

    first complete test

--- aggregator/engine.py
+++ aggregator/engine.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import datetime
+from populate_table import insert_data
 from netatmo.welcome_api import get_number_of_people, is_user_present
 from tv.lg_api import is_tv_on
 from hue.hue_api import get_main_light_status, get_secondary_light_status, is_main_light_on, is_secondary_light_on, \
@@ -15,6 +16,20 @@ def get_hour():
     return datetime.datetime.now().hour
 
 
+def get_dictionary():
+    return {
+        'is_user_at_home': is_user_present('Gianmarco'),
+        'n_of_people': get_number_of_people(),
+        'light_1_status': is_main_light_on(),
+        'light_1_color': get_main_light_status(),
+        'light_2_status': is_secondary_light_on(),
+        'light_2_color': get_secondary_light_status(),
+        'tv_status': is_tv_on(),
+        'day': get_day_of_week(),
+        'hour': get_hour(),
+    }
+
+
 def __main__():
     print 'is user present: %s' % is_user_present('Gianmarco')
     print '    n of people: %s' % get_number_of_people()
@@ -26,6 +41,6 @@ def __main__():
     print '          TV on: %s' % is_tv_on()
     print '            Day: %s' % get_day_of_week()
     print '           Hour: %s' % get_hour()
-
+    insert_data(get_dictionary())
 
 __main__()
--- database/tables.sql
+++ database/tables.sql
@@ -7,14 +7,13 @@ CREATE DATABASE home;
 
 CREATE TABLE IF NOT EXISTS data (
   id SERIAL,
-  hour              integer     not null,
-  day               varchar     not null,
-  tv_status         boolean     not null,
-  screen_status     boolean     not null,
-  light_1_status    boolean     not null,
-  light_2_status    boolean     not null,
-  light_1_intensity float       not null,
-  light_2_intensity float       not null,
-  light_1_color     float       not null,
-  light_2_color     float       not null
+  hour               integer       not null,
+  day                varchar       not null,
+  is_user_at_home    boolean       not null,
+  n_of_people        integer       not null,
+  tv_status          boolean       not null,
+  light_1_status     boolean       not null,
+  light_2_status     boolean       not null,
+  light_1_color      varchar       not null,
+  light_2_color      varchar       not null
 );
